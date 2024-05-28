cbedef33565d12b22f585763cc1118a933200eee
Seshi Reddy Syamala <seshireddy@usf.edu>
2024-05-28 15:57:22 -0400

    Rename .gitignore and add untracked files

---
+++ css/style.cs
@@ -0,0 +1,43 @@
+﻿body {
+    font-family: Arial, sans-serif;
+margin: 0;
+padding: 0;
+box - sizing: border - box;
+}
+
+header {
+    background-color: #333;
+    color: #fff;
+    padding: 1rem 0;
+}
+
+nav ul
+{
+    list-style: none;
+    padding: 0;
+    display: flex;
+    justify-content: space-around;
+}
+
+nav ul li a {
+    color: #fff;
+    text - decoration: none;
+}
+
+main {
+    padding: 2rem;
+}
+
+section {
+    margin-bottom: 2rem;
+}
+
+footer {
+    background-color: #333;
+    color: #fff;
+    text - align: center;
+padding: 1rem 0;
+position: fixed;
+bottom: 0;
+width: 100 %;
+}
