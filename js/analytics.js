7a89f46b7cf00ae2029d0d552622563c1aebabc8
Seshi Reddy Syamala <seshireddy@usf.edu>
2024-05-28 20:02:37 -0400

    Updates

--- js/analytics.js
+++ js/analytics.js
@@ -22,7 +22,9 @@ document.addEventListener('DOMContentLoaded', () => {
                     'rgba(255, 206, 86, 1)',
                     'rgba(75, 192, 192, 1)',
                     'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64,],
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(54, 162, 235, 1)'
+                ],
                 borderWidth: 1
             }]
         },
@@ -34,4 +36,4 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     });
-}); 
+});
