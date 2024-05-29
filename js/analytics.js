66fca3e3f38137f0cc2d0d23c7c9b1b81646e4ad
Seshi Reddy Syamala <seshireddy@usf.edu>
2024-05-29 01:47:27 -0400

    upgrades

--- js/analytics.js
+++ js/analytics.js
@@ -62,7 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     'rgba(75, 192, 192, 0.2)',
                     'rgba(153, 102, 255, 0.2)',
                     'rgba(255, 159, 64, 0.2)',
-                    'rgba(54, 162, 235, 0.2)'
+                    'rgba(199, 199, 199, 0.2)'
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
@@ -71,10 +71,16 @@ document.addEventListener('DOMContentLoaded', () => {
                     'rgba(75, 192, 192, 1)',
                     'rgba(153, 102, 255, 1)',
                     'rgba(255, 159, 64, 1)',
-                    'rgba(54, 162, 235, 1)'
-                ],
-                borderWidth: 1
+                    'rgba(199, 199, 199, 1)'
+                ]
             }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
         }
     });
-});
+
